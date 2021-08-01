be2f3acb1f15d17797643ceed1c475cf83983826
20012645 <20012645@LAPTOP-PIJ7L4G3>
2021-08-02 00:45:34 +0800

    Edit stalls class

--- C206_CaseStudy/src/Stalls.java
+++ C206_CaseStudy/src/Stalls.java
@@ -1,4 +1,43 @@
+import java.util.Date;
 
 public class Stalls {
+	private int stallID;
+	private int stallName;
+	private Date operationDate;
 
+	public Stalls(int stallID, int stallName, Date operationDate) {
+		this.stallID = stallID;
+		this.stallName = stallName;
+		this.operationDate = operationDate;
+	}
+
+	public int getStallID() {
+		return stallID;
+	}
+
+	public void setStallID(int stallID) {
+		this.stallID = stallID;
+	}
+
+	public int getStallName() {
+		return stallName;
+	}
+
+	public void setStallName(int stallName) {
+		this.stallName = stallName;
+	}
+
+	public Date getOperationDate() {
+		return operationDate;
+	}
+
+	public void setOperationDate(Date operationDate) {
+		this.operationDate = operationDate;
+	}
+	
+	
+	
+	
 }
+
+
