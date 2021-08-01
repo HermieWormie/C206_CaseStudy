3b83b8ec989d411d67fcff09046c4fba78b84117
herma <herma@Herman>
2021-08-01 23:49:50 +0800

    update

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -3,38 +3,38 @@ import java.util.ArrayList;
 public class C206_CaseStudy {
 
 	private static ArrayList<Promotions> promoList = new ArrayList<Promotions>();
-	
+
 	public static void main(String[] args) {
-		//Food Items
+		// Food Items
 		ArrayList<FoodItems> itemsList = new ArrayList<FoodItems>();
 		itemsList.add(new FoodItems(1, "Grilled Chicken Chop", 4));
 		itemsList.add(new FoodItems(2, "Fried Chicken Cutlet", 6));
-		
-		//Promotions
-		promoList.add(new Promotions(1, 6,"Laksa", 20, "Monday"));
-		promoList.add(new Promotions(2, 4,"Nasi Lemak", 10, "Tuesday"));
-		
+
+		// Promotions
+		promoList.add(new Promotions(1, 6, "Laksa", 20, "Monday"));
+		promoList.add(new Promotions(2, 4, "Nasi Lemak", 10, "Tuesday"));
+
 		int option = 0;
 
 		while (option != 6) {
 
 			menu();
 			option = Helper.readInt("Enter an option > ");
-			
+
 			if (option == 1) {
-				//Stalls
+				// Stalls
 				manageStalls();
 			} else if (option == 2) {
-				//Food Items
+				// Food Items
 				manageFoodItems(itemsList);
 			} else if (option == 3) {
-				//Purchase Orders
+				// Purchase Orders
 				managePurchaseOrders();
 			} else if (option == 4) {
-				//Promotions
+				// Promotions
 				managePromotions();
 			} else if (option == 5) {
-				//Orders
+				// Orders
 				manageOrders();
 			} else if (option == 6) {
 				System.out.println("Thanks for using this application!");
@@ -62,14 +62,19 @@ public class C206_CaseStudy {
 		System.out.println(header);
 		Helper.line(80, "-");
 	}
-	
-	// ====================================================================== FOR MANAGE STALLS (DON) ===============================================================================
+
+	// ====================================================================== FOR
+	// MANAGE STALLS (DON)
+	// ===============================================================================
 	public static void manageStalls() {
-		
+
 	}
-	// ================================================================== FOR MANAGE FOOD ITEMS (WANYING) ============================================================================
+
+	// ================================================================== FOR MANAGE
+	// FOOD ITEMS (WANYING)
+	// ============================================================================
 	public static void manageFoodItems(ArrayList<FoodItems> itemsList) {
-				
+
 		int choice = -1;
 		while (choice != 4) {
 			setHeader("Manage Food Items");
@@ -81,71 +86,68 @@ public class C206_CaseStudy {
 			System.out.println();
 			if (choice == 1) {
 				viewFoodItems(itemsList);
-			}
-			else if (choice == 2) {
+			} else if (choice == 2) {
 				FoodItems fi = inputFoodItems(itemsList);
 				addFoodItems(itemsList, fi);
-			}
-			else if (choice == 3) {
+			} else if (choice == 3) {
 				viewFoodItems(itemsList);
 				deleteFoodItems(itemsList);
-			}
-			else if (choice == 4) {
+			} else if (choice == 4) {
 				System.out.println("Back to Home");
-			}
-			else {
+			} else {
 				System.out.println("Invalid choice. Please try again");
 			}
 		}
 	}
-	//-------------------------------------------------------------------------------------
+
+	// -------------------------------------------------------------------------------------
 	public static String retrieveAllFoodItems(ArrayList<FoodItems> itemsList) {
 		String output = "";
-		
+
 		for (FoodItems fi : itemsList) {
 			output += String.format("%-10d %-25s $%d\n", fi.getFoodId(), fi.getFoodName(), fi.getFoodPrice());
 		}
 		return output;
 	}
-	
+
 	public static void viewFoodItems(ArrayList<FoodItems> itemsList) {
 		String output = String.format("%-10s %-25s %s\n", "FOOD ID", "NAME", "PRICE");
 		if (itemsList.size() == 0) {
 			output = "There are no food items available.";
-		}
-		else {
+		} else {
 			output += retrieveAllFoodItems(itemsList);
 		}
 		System.out.println(output);
 	}
-	//-------------------------------------------------------------------------------------
+
+	// -------------------------------------------------------------------------------------
 	public static FoodItems inputFoodItems(ArrayList<FoodItems> itemsList) {
 		String name = Helper.readString("Enter Food Name > ");
 		int price = Helper.readInt("Enter Food Price > ");
-		
+
 		if (price > 2 && price < 16) {
 			FoodItems fi = new FoodItems(itemsList.size() + 1, name, price);
 			return fi;
-		}
-		else {
+		} else {
 			return null;
 		}
 	}
+
 	public static void addFoodItems(ArrayList<FoodItems> itemsList, FoodItems fi) {
 		if (fi == null) {
 			System.out.println("Food price limit ranges from $3 - $15. Please try again.");
-		}
-		else {
+		} else {
 			itemsList.add(fi);
 			System.out.println(String.format("Food Item added!\n"));
 		}
-		
+
 	}
-	//-------------------------------------------------------------------------------------
+
+	// -------------------------------------------------------------------------------------
 	public static void deleteFoodItems(ArrayList<FoodItems> itemsList) {
 		int id = Helper.readInt("Enter Food ID to delete > ");
 		boolean match = false;
-		for (int i = 0 ; i < itemsList.size(); i++) {
+		for (int i = 0; i < itemsList.size(); i++) {
 			if (itemsList.get(i).getFoodId() == id) {
 				itemsList.remove(i);
 				match = true;
@@ -153,27 +155,31 @@ public class C206_CaseStudy {
 		}
 		if (!match) {
 			System.out.println("Invalid Food ID entered!");
-		}
-		else {
+		} else {
 			for (FoodItems fi : itemsList) {
 				if (fi.getFoodId() > id) {
-					fi.setFoodId(fi.getFoodId()-1);
+					fi.setFoodId(fi.getFoodId() - 1);
 				}
 			}
-			System.out.println("Food ID " + id+ " Deleted!");
+			System.out.println("Food ID " + id + " Deleted!");
 		}
-		
+
 	}
-	
-	// ================================================================== FOR MANAGE FOOD ITEMS (EDISON) ============================================================================
+
+	// ================================================================== FOR MANAGE
+	// FOOD ITEMS (EDISON)
+	// ============================================================================
 	public static void managePurchaseOrders() {
-	
+
 	}
-	// ================================================================== FOR MANAGE FOOD ITEMS (HERMAN) ============================================================================
+
+	// ================================================================== FOR MANAGE
+	// FOOD ITEMS (HERMAN)
+	// ============================================================================
 	public static void managePromotions() {
 		int choice = -1;
 		while (choice != 4) {
-	
+
 			setHeader("Manage Promotions");
 			System.out.println("1. View Promotions");
 			System.out.println("2. Add Promotion");
@@ -183,30 +189,15 @@ public class C206_CaseStudy {
 			System.out.println();
 			if (choice == 1) {
 				viewPromotions();
-			}
-			else if (choice == 2) {
-				String name = Helper.readString("Enter Food Name > ");
-				int stall = Helper.readInt("Enter Stall ID > ");
-				int percentage = Helper.readInt("Enter discount percentage > ");
-				String day = Helper.readString("Enter day of the week > ");
-				
-				for(int i = 0; i < promoList.size(); i++) {
-					if(stall == promoList.get(i).getStallID() && day.equalsIgnoreCase(promoList.get(i).getDOTW())) {
-						System.out.println("A stall can only have 1 promotion for 1 food item per day");
-					}
-					else {
-						promoList.add(new Promotions(promoList.size() + 1, stall, name, percentage, day));
-						System.out.println(String.format("Promotion for " + name + " at stall number " + stall + " on "+ day + " added!\n"));
-					}
-				}
-				
-			}
-			else if (choice == 3) {
+			} else if (choice == 2) {
+				Promotions pr = inputPromoItems(promoList);
+				addPromotionItems(promoList, pr);
+			} else if (choice == 3) {
 				viewPromotions();
 				int id = Helper.readInt("Enter Promotion ID to delete > ");
 				int flag = -1;
-				
-				for (int i = 0 ; i < promoList.size(); i++) {
+
+				for (int i = 0; i < promoList.size(); i++) {
 					if (promoList.get(i).getPromotionID() == id) {
 						promoList.remove(i);
 						flag = 1;
@@ -214,41 +205,72 @@ public class C206_CaseStudy {
 				}
 				if (flag == -1) {
 					System.out.println("Invalid Promotion ID entered!");
-				}
-				else {
+				} else {
 					for (Promotions pr : promoList) {
 						if (pr.getPromotionID() > id) {
-							pr.setPromotionID(pr.getPromotionID()-1);
+							pr.setPromotionID(pr.getPromotionID() - 1);
 						}
 					}
-					System.out.println("Food ID " + id+ " Deleted!");
+					System.out.println("Food ID " + id + " Deleted!");
 				}
-				
-			}
-			else if (choice == 4) {
+
+			} else if (choice == 4) {
 				System.out.println("Back to Home");
-			}
-			else {
+			} else {
 				System.out.println("Invalid choice. Please try again");
 			}
 		}
 	}
-	
+
 	public static void viewPromotions() {
-		String output = String.format("%-15s %-14s %-16s %-10s %s\n", "PROMOTION ID", "STALL NUMBER",  " FOOD ITEM", "OFFER(%)", "DAY OF THE WEEK");
-		
+		String output = String.format("%-15s %-14s %-16s %-10s %s\n", "PROMOTION ID", "STALL NUMBER", " FOOD ITEM",
+				"OFFER(%)", "DAY OF THE WEEK");
+
 		for (Promotions pr : promoList) {
 			if (promoList.size() == 0) {
 				output = "There are no food items available.";
-			}
-			else {
-				output += String.format("%-15d %-15d %-15s %-10d %s\n", pr.getPromotionID(), pr.getStallID(), pr.getfoodName(), pr.getPromotionPercent(), pr.getDOTW());
+			} else {
+				output += String.format("%-15d %-15d %-15s %-10d %s\n", pr.getPromotionID(), pr.getStallID(),
+						pr.getfoodName(), pr.getPromotionPercent(), pr.getDOTW());
 			}
 		}
 		System.out.println(output);
 	}
-	// ================================================================== FOR MANAGE FOOD ITEMS (SARA) ============================================================================
+
+	public static Promotions inputPromoItems(ArrayList<Promotions> promoList) {
+		String name = Helper.readString("Enter Food Name > ");
+		int stall = Helper.readInt("Enter Stall ID > ");
+		int percentage = Helper.readInt("Enter discount percentage > ");
+		String day = Helper.readString("Enter day of the week > ");
+		boolean flag = false;
+		Promotions pr = new Promotions(promoList.size() + 1, stall, name, percentage, day);
+
+		for (int i = 0; i < promoList.size(); i++) {
+			if (!(promoList.get(i).getStallID() == stall) && !(day.equalsIgnoreCase(promoList.get(i).getDOTW()))) {
+				flag = true;
+				return pr;
+			}
+		}
+		if (flag = false) {
+			 pr = null;
+		}
+		return pr;
+	}
+
+	public static void addPromotionItems(ArrayList<Promotions> promoList, Promotions pr) {
+		if (pr == null) {
+			System.out.println("Each stall can only have one promotion at a time!");
+		} else {
+			promoList.add(pr);
+			System.out.println(String.format("Promotion added!\n"));
+		}
+
+	}
+
+	// ================================================================== FOR MANAGE
+	// FOOD ITEMS (SARA)
+	// ============================================================================
 	public static void manageOrders() {
-		
+
 	}
 }
--- C206_CaseStudy/test/PromotionsTest.java
+++ C206_CaseStudy/test/PromotionsTest.java
@@ -1,22 +1,50 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class PromotionsTest {
 
-	@Before
-	public void setUp() throws Exception {
+	private Promotions p1;
+	private Promotions p2;
+
+	private ArrayList<Promotions> promoList;
+
+	public PromotionsTest() {
+		super();
 	}
 
+	public void setUp() throws Exception {
+		p1 = new Promotions(1, 6, "Laksa", 20, "Monday");
+		p2 = new Promotions(2, 4, "Nasi Lemak", 10, "Tuesday");
+
+		promoList = new ArrayList<Promotions>();
+	}
+
+	@Before
+	public void testAddPromotions() {
+		assertNotNull("Test if there is valid Food Items arraylist to add to", promoList);
+
+		// Given an empty list, after adding 1 item, the size of the list is 1
+		C206_CaseStudy.addPromotionItems(promoList, p1);
+		assertEquals("Test if that Food Items arraylist size is 1?", 1, promoList.size());
+
+		// The item just added is as same as the first item of the list
+		assertSame("Test that Food Items is added same as 1st item of the list?", p1, promoList.get(0));
+
+		// Add another item. test The size of the list is 2?
+		C206_CaseStudy.addPromotionItems(promoList, p2);
+		assertEquals("Test that Food Items arraylist size is 2?", 2, promoList.size());
+	}
 
 	@Test
 	public void c206_test() {
-		//fail("Not yet implemented"); 
-		assertTrue("C206_CaseStudy_SampleTest ",true);
+
 	}
-	
+
 	@After
 	public void tearDown() throws Exception {
 	}
