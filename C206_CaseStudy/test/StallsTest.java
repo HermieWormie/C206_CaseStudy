492138aacc475c3c96c89c48978ef2821ff7a624
wanyi <wanyi@DESKTOP-2KE03H2>
2021-08-01 19:45:53 +0800

    all test files

---
+++ C206_CaseStudy/test/StallsTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StallsTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+
+	@Test
+	public void c206_test() {
+		//fail("Not yet implemented"); 
+		assertTrue("C206_CaseStudy_SampleTest ",true);
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
