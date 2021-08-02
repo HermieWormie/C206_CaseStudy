c8da6800633e493f97c43f564055c4709148b225
20022539 <20022539@saras-laprop>
2021-08-02 14:31:33 +0800

    test

--- C206_CaseStudy/src/Orders.java
+++ C206_CaseStudy/src/Orders.java
@@ -5,6 +5,12 @@ public class Orders {
 	private int foodId;
 	private int foodPrice;
 	private int description;
+	public int getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
 	
 
 
