7867606eb458d4832ad2999c8a6e3e317b5b2635
wanyi <wanyi@DESKTOP-2KE03H2>
2021-08-08 00:27:36 +0800

    edit foodItem test cases

--- C206_CaseStudy/test/FoodItemsTest.java
+++ C206_CaseStudy/test/FoodItemsTest.java
@@ -69,27 +69,22 @@ public class FoodItemsTest {
 	
 	@Test
 	public void testDeleteFoodItems() {
-		// Test if Item list is not null but empty, so that can add a new item
+				
+		//check if foodlist item is not null, so can delete an item
 		assertNotNull("Test if there is valid Food Items arraylist to add to", itemsList);
-
-		// test if the list of Food Items retrieved from the SourceCentre is empty
-		String allFoodItems= C206_CaseStudy.retrieveAllFoodItems(itemsList);;
-		String testOutput = "";
-		assertEquals("Check that ViewAllFoodItemslist", testOutput, allFoodItems);
-
-		// Given an empty list, after adding 2 items, test if the size of the list is 2
+		
+		//after adding 2 items check the foodlist size is 2
 		C206_CaseStudy.addFoodItems(itemsList, fi1);
 		C206_CaseStudy.addFoodItems(itemsList, fi2);
 		assertEquals("Test if that Camcorder arraylist size is 2?", 2, itemsList.size());
-
-		// test if the expected output string same as the list of FoodItems retrieved
-		// from the SourceCentre
-		allFoodItems = C206_CaseStudy.retrieveAllFoodItems(itemsList);
-
-		testOutput = String.format("%-10d %-25s $%d\n", 1, "Grilled Chicken Chop", 4);
-		testOutput += String.format("%-10d %-25s $%d\n", 1, "Fried Chicken Cutlet", 6);
-
-		assertEquals("Check that ViewAllFoodItems", testOutput, allFoodItems);		
+		
+		//after removing one item, check size of foodlist becomes 1
+		itemsList.remove(0);
+		assertEquals("Test if that Camcorder arraylist size is 1?", 1, itemsList.size());
+		
+		//remove another item, list size becomes 0
+		itemsList.remove(0);
+		assertEquals("Test if that Camcorder arraylist size is 0?", 0, itemsList.size());
 	}
 	
 	@After
