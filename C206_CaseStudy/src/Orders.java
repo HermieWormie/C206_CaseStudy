5409fea2dc4a9d256fafc93d45ca502aae8d4614
20022539 <20022539@saras-laprop>
2021-08-07 20:18:42 +0800

    updated order class

--- C206_CaseStudy/src/Orders.java
+++ C206_CaseStudy/src/Orders.java
@@ -1,19 +1,46 @@
+import java.util.ArrayList;
 
 public class Orders {
 	
-	private int orderId;
-	private int foodId;
-	private int foodPrice;
-	private int description;
-	public int getOrderId() {
-		return orderId;
+	private String customerId;
+	private String orderDate;
+	private ArrayList<FoodItems> items;
+	
+	public Orders(String customerId, String orderDate, ArrayList<FoodItems> items) {
+		super();
+		this.customerId = customerId;
+		this.orderDate = orderDate;
+		this.items = items;
 	}
-	public void setOrderId(int orderId) {
-		this.orderId = orderId;
+
+	public String getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(String customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(String orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public ArrayList<FoodItems> getItems() {
+		return items;
+	}
+
+	public void setItems(ArrayList<FoodItems> items) {
+		this.items = items;
 	}
 	
-
-
-	
+	public String toString() {
+		String output = "";
+		output += String.format("%-15s %-20s %-15s", customerId, orderDate, items);
+		return output;
+	}
 	
 }
